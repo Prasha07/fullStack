24a0e0730166b39bbfb0fc76c158b41f5bad7a3c
Prasha07 <prashantshukla0710@gmail.com>
2025-01-26 21:37:35 +0530

    "header fe "

--- Angular/E_commerce_FrontEnd/src/app/app.component.ts
+++ Angular/E_commerce_FrontEnd/src/app/app.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HeaderComponent } from "./header/header.component";
+import { CarouselComponent } from "./carousel/carousel.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, HeaderComponent, CarouselComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
